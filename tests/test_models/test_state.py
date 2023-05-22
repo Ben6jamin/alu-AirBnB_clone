f0761dfc8ed15b8dfba6566a4c009cb2a065818c
M-Umwari <m.umwari@alustudent.com>
2023-05-22 16:15:55 +0200

    testing

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,13 +1,38 @@
 #!/usr/bin/python3
+
+
+"""Unittest for State Class."""
+
 import unittest
+
 from models.state import State
 
+from models.base_model import BaseModel
 
-class TestBase(unittest.TestCase):
-    def test_initialization(self):
+
+class TestState(unittest.TestCase):
+    """Test cases State class."""
+
+    def test_instance(self):
+        """test instance."""
         state = State()
-        self.assertEqual(
-            str(type(state)), "<class 'models.state.State'>")
+        self.assertIsInstance(state, State)
+
+    def test_is_class(self):
+        """test instance."""
+        state = State()
+        self.assertEqual(str(type(state)),
+                         "<class 'models.state.State'>")
+
+    def test_is_subclass(self):
+        """test is_subclass."""
+        state = State()
+        self.assertTrue(issubclass(type(state), BaseModel))
+
+    def test_attr(self):
+        """test is_subclass."""
+        state = State()
+        self.assertIsNotNone(state.id)
         self.assertEqual(state.name, "")
 
 
